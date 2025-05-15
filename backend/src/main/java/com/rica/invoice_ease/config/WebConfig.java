981bf7de8ebf1d667e91c451584386d4a6ac7596
Ricardo Tesla <ngarica1603@gmail.com>
2025-05-15 07:43:35 +0200

    Updated project for deployment

--- backend/src/main/java/com/rica/invoice_ease/config/WebConfig.java
+++ backend/src/main/java/com/rica/invoice_ease/config/WebConfig.java
@@ -9,8 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500",
-                               "https://get-invoice-eight.vercel.app/") // Update with your frontend URL
+                .allowedOrigins("http://127.0.0.1:5500") // Update with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
